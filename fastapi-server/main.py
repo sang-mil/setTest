ea46e2d08ed0670d83772231045c1d81abdf94c3
sang-mil <sangmil6369@gmail.com>
2026-03-08 10:34:38 +0000

    cors

--- fastapi-server/main.py
+++ fastapi-server/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from sqlalchemy import create_engine
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
